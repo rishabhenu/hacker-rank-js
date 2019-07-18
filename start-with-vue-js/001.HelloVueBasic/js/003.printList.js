2f1afcaa749c51ba13db767e914c34a08e8b7555
rishabhenu <rishabhenu@gmail.com>
2019-07-18 15:18:09 +0530

    basic programs using vue.js

---
+++ start-with-vue-js/001.HelloVueBasic/js/003.printList.js
@@ -0,0 +1,14 @@
+new Vue({
+    el: "#empData",
+    data: {
+        employees: [{
+            sNo: 1,
+            name: "Rishabh Sharma",
+            designation: "Software Developer"
+        }, {
+            sNo: 2,
+            name: "Arnab Chatterjee",
+            designation: "Software Developer"
+        }]
+    }
+})
